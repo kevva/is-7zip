f163b89be856374dca450a1f5f8a50bd1b3e1b3c
Kevin Martensson <kevinmartensson@gmail.com>
2015-01-28 17:19:14 +0100

    Various tweaks

--- test/test.js
+++ test/test.js
@@ -6,10 +6,10 @@ var readChunk = require('read-chunk');
 var test = require('ava');
 
 test('should detect 7ZIP from buffer', function (t) {
-    t.plan(2);
+	t.plan(2);
 
-    readChunk(path.join(__dirname, 'fixtures/test.7z'), 0, 6, function (err, buf) {
-        t.assert(!err);
-        t.assert(is7zip(buf));
-    });
+	readChunk(path.join(__dirname, 'fixtures/test.7z'), 0, 6, function (err, buf) {
+		t.assert(!err, err);
+		t.assert(is7zip(buf));
+	});
 });
